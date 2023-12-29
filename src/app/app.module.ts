0f7a6c086871945e933db2cbf797d14a7761740d
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2023-12-28 21:40:12 -0300

    registerNewUser and basic footer templates created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterUserComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
